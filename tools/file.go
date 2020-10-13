1a4c6181673f24903cce87ce55d038a5720f71ab
mo <thinkgo@aliyun.com>
2020-10-13 21:55:15 +0800

    TODO: remove cache

--- tools/file.go
+++ tools/file.go
@@ -4,54 +4,10 @@ import (
 	"bufio"
 	"context"
 	"io"
-	"io/ioutil"
 	"log"
 	"os"
-	"path/filepath"
-	"strings"
 	"time"
 )
-
-type ReplaceHelper struct {
-	Root    string // 路径
-	OldText string // 需要替换的文本
-	NewText string // 新的文本
-}
-
-func (h *ReplaceHelper) DoWrok() error {
-	return filepath.Walk(h.Root, h.walkCallback)
-}
-
-func (h ReplaceHelper) walkCallback(path string, f os.FileInfo, err error) error {
-	if err != nil {
-		return err
-	}
-	if f == nil {
-		return nil
-	}
-	if f.IsDir() {
-		log.Println("DIR:", path)
-		return nil
-	}
-
-	// 文件类型需要进行过滤
-	buf, err := ioutil.ReadFile(path)
-	if err != nil {
-		// err
-		return err
-	}
-	content := string(buf)
-	log.Printf("h.OldText: %s \n", h.OldText)
-	log.Printf("h.NewText: %s \n", h.NewText)
-
-	// 替换
-	newContent := strings.Replace(content, h.OldText, h.NewText, -1)
-
-	// 重新写入
-	ioutil.WriteFile(path, []byte(newContent), 0)
-
-	return err
-}
 
 func FileMonitoringById(ctx context.Context, filePth string, id string, group string, hookfn func(context.Context, string, string, []byte)) {
 	f, err := os.Open(filePth)
@@ -61,7 +17,7 @@ func FileMonitoringById(ctx context.Context, filePth string, id string, group st
 	defer f.Close()
 
 	rd := bufio.NewReader(f)
-	f.Seek(0, 2)
+	f.Seek(0, io.SeekEnd) // nolint errcheck
 	for {
 		if ctx.Err() != nil {
 			break
