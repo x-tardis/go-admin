3479e66f9cae73e59dbf52f487814622e03647ac
mo <thinkgo@aliyun.com>
2020-11-09 15:03:19 +0800

    update docs

--- _testing/test/main.go
+++ _testing/test/main.go
@@ -1,16 +1,17 @@
 package main
 
 import (
-	"github.com/x-tardis/go-admin/deployed/dao"
-	"github.com/x-tardis/go-admin/pkg/database"
+	"log"
+
+	"github.com/casbin/casbin/v2/util"
 )
 
 func main() {
-	dao.SetupDatabase(&database.Database{
-		"mysql",
-		"root:catmaotu@tcp(127.0.0.1:3306)/goadmin?charset=utf8&parseTime=True&loc=Local&timeout=1000ms",
-		true,
-	})
+	// dao.SetupDatabase(&database.Database{
+	// 	"mysql",
+	// 	"root:catmaotu@tcp(127.0.0.1:3306)/goadmin?charset=utf8&parseTime=True&loc=Local&timeout=1000ms",
+	// 	true,
+	// })
 
 	// err := trans.Exec(context.Background(), dao.DB, func(ctx context.Context) error {
 	// 	err := models.CRole.BatchDelete(ctx, []int{2})
@@ -20,4 +21,6 @@ func main() {
 	// 	return models.CUser.BatchDelete(ctx, []int{3})
 	// })
 	// log.Println(err)
+
+	log.Println(util.KeyMatch2("/abc/cde", "/:id"))
 }
